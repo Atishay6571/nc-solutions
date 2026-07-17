e88d9bcb48a273126af701db91715e511bc51fad
Atishay6571 <144715210+Atishay6571@users.noreply.github.com>
2026-07-18 00:08:55 +0530

    Add: ipo - submission-2

---
+++ Data Structures & Algorithms/ipo/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        #need to look at all projects available in the current capital
+        #use a MIN HEAP
+
+        capital=[[capital[project],project] for project in range(len(capital))]
+        heapq.heapify(capital)
+        totalProfits=[]
+        heapq.heapify(totalProfits)
+
+        for i in range(k):
+            while capital and capital[0][0]<=w:
+                element=heapq.heappop(capital)
+                project=element[1]
+                heapq.heappush(totalProfits,[-profits[project],project])
+
+            if totalProfits==[]:
+                return w
+            #then choose the maximum profit among those
+            #use a MAX HEAP
+
+            gain=-heapq.heappop(totalProfits)[0]
+            w+=gain
+        return w
+
