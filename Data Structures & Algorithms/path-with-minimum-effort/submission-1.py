7d371f4a14f31d7840a4d79f02b369cd115b73a4
Atishay <144715210+Atishay6571@users.noreply.github.com>
2026-07-24 21:24:04 +0530

    Add: path-with-minimum-effort - submission-1

---
+++ Data Structures & Algorithms/path-with-minimum-effort/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        row,col=len(heights),len(heights[0])
+        #djikstra's algo: track the minimum effort to reach a cell
+        minHeap=[(0,0,0)] #[effort, row, col] 
+        directions=[(-1,0),(1,0),(0,1),(0,-1)]
+
+        #dynamically build heap
+        #DJIKSTRA: the first time a node is popped thats the minimum.
+        #so once a node is processed, no need to process it ever again
+        visited=set()
+        while minHeap:
+            cell= heapq.heappop(minHeap)
+            r,c=cell[1],cell[2]
+            if (r,c) in visited:
+                continue
+            visited.add((r,c))
+
+            if r==row-1 and c==col-1:
+                return cell[0]
+            for dr,dc in directions:
+                newr,newc=r+dr,c+dc
+                if (newr<0 or newc<0 or newr>=row or newc>=col
+                     or (newr,newc) in visited ):
+                    continue
+                effort=abs(heights[r][c]-heights[newr][newc])
+                effort=max(effort,cell[0])
+                heapq.heappush(minHeap, (effort,newr,newc))
+            
+
+                
+
