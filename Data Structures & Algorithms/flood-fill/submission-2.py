c9972f522bff695fa756c799e2d72ad69a5cf032
Atishay <144715210+Atishay6571@users.noreply.github.com>
2026-08-02 19:02:29 +0530

    Add: flood-fill - submission-2

---
+++ Data Structures & Algorithms/flood-fill/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        directions=[(0,1),(0,-1),(1,0),(-1,0)]
+        rows, cols = len(image), len(image[0])
+        original= image[sr][sc]
+        def dfs(r,c):
+            image[r][c]= color
+            for dr, dc in directions:
+                nr, nc= dr+r, dc+ c
+                if (nr<0 or nc<0 or nr>= rows or nc >= cols or image[nr][nc]!= original or image[nr][nc]==color):
+                    continue
+                dfs(nr,nc)
+        dfs(sr,sc)
+        return image
+            
