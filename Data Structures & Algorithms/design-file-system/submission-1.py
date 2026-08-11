ece8787ff5a90205347cfe2719596bf8957e09e1
Atishay <144715210+Atishay6571@users.noreply.github.com>
2026-08-11 20:47:19 +0530

    Add: design-file-system - submission-1

---
+++ Data Structures & Algorithms/design-file-system/submission-1.py
@@ -0,0 +1,22 @@
+class FileSystem:
+
+    def __init__(self):
+        self.hmap = {"": 0} # maps values to their values
+
+    def createPath(self, path: str, value: int) -> bool:
+        part = path.rsplit('/',1 )
+        if part[1] == "" or part[0] not in self.hmap or path in self.hmap:
+            return False
+        self.hmap[path] = value
+        return True
+
+    def get(self, path: str) -> int:
+        if path in self.hmap:
+            return self.hmap[path]
+        return -1        
+
+
+# Your FileSystem object will be instantiated and called as such:
+# obj = FileSystem()
+# param_1 = obj.createPath(path,value)
+# param_2 = obj.get(path)
