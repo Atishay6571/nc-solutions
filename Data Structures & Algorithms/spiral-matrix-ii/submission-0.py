eacbb957d32c24e98b00181584bb08b8c11fce14
Atishay <144715210+Atishay6571@users.noreply.github.com>
2026-08-11 21:20:49 +0530

    Add: spiral-matrix-ii - submission-0

---
+++ Data Structures & Algorithms/spiral-matrix-ii/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        rows = cols = n
+        matrix = [[0]*rows for i in range(cols) ]
+        # a function to complete the row/col with direction
+        def complete(r,c, dr, dc):
+            nr, nc = r+ dr, c+dc
+            prev = matrix[r][c] # already filles
+            while (nr>=0 and nc>=0 and nr<rows and nc <cols and matrix[nr][nc]==0):
+                matrix[nr][nc] = prev+1
+                prev+=1
+                nr+=dr
+                nc+=dc 
+            return nr-dr,nc-dc
+        
+        r,c =0,0
+        matrix[r][c]=1
+
+        for i in range(n**2):
+            r,c = complete(r,c, 0, 1) # right
+            r,c = complete(r,c, 1, 0 ) # down
+            r,c = complete(r,c, 0, -1) # left
+            r,c = complete(r,c, -1, 0) # up
+
+        return matrix
